18c014e01112f5ddf3047c2121aa5d3ac232179b
Anders <ahpyenson@gmail.com>
2022-04-09 09:50:11 -0400

    changed transition time

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,9 @@
 
 <script>
 
+  // TODO: Add dark mode code to about and cases. Also, change button text to
+  // dark/light depending on isdark value.
+
   import {theme} from '../stores.js';
   import { onMount } from 'svelte';
 
@@ -136,7 +139,7 @@
 	}
 
   :global(.bodytransition) {
-    transition: --bg-color, 0.2s;
+    transition: --bg-color, 0.3s;
   }
   .hideMe {
     visibility:hidden;
